ae704a615433a9fab8009d6bd17f472f50c6afa5
wiliamdecosta <wiliamdecosta@gmail.com>
2016-05-09 12:42:00 +0700

    ''

--- application/libraries/Maintenance_controller.php
+++ application/libraries/Maintenance_controller.php
@@ -7,14 +7,14 @@
 class Maintenance_controller {
 
     function read() {
-		
+
 		$start = getVarClean('start','int',0);
     	$limit = getVarClean('limit','int',50);
 
     	$sort = getVarClean('sort', 'str', 'mnt_id');
         $dir = getVarClean('dir', 'str', 'DESC');
         $query = getVarClean('query', 'str', '');
-        
+
         $mnt_id = getVarClean('mnt_id', 'int', 0);
 		$app_id = getVarClean('app_id', 'int', 0);
 		$mnt_desc = getVarClean('mnt_desc', 'str', '');
@@ -25,16 +25,20 @@ class Maintenance_controller {
 		$mnt_created_by = getVarClean('mnt_created_by', 'str', '');
 		$mnt_updated_date = getVarClean('mnt_updated_date', 'date', '');
 		$mnt_updated_by = getVarClean('mnt_updated_by', 'str', '');
-		
+
 		$search_text = getVarClean('search_text', 'str', '');
+
+        $start_date = getVarClean('start_date', 'str', '');
+        $end_date = getVarClean('end_date', 'str', '');
+
     	$data = array('items' => array(), 'success' => false, 'message' => '');
 
     	try {
-            
+
             $ci = & get_instance();
 		    $ci->load->model('maintenance');
 		    $table = $ci->maintenance;
-		    
+
 		    //Set default criteria. You can override this if you want
             foreach ($table->fields as $key => $field){
                 if (!empty($$key)){ // <-- Perhatikan simbol $$
@@ -45,23 +49,31 @@ class Maintenance_controller {
                     }
                 }
             }
-            
+
             if(!empty($search_text)) {
                 $table->setCriteria("maintenance.mnt_desc ILIKE '%".$search_text."%'
                                         OR maintenance.mnt_by ILIKE '%".$search_text."%'
                                         OR maintenance.mnt_evidence_desc ILIKE '%".$search_text."%'");
             }
-            
+
+            if(!empty($start_date) && !empty($end_date)) {
+                $table->setCriteria("(maintenance.mnt_date between '".$start_date."' and '".$end_date."')");
+            }elseif(!empty($start_date)) {
+                $table->setCriteria("maintenance.mnt_date = '".$start_date."'");
+            }elseif(!empty($end_date)) {
+                $table->setCriteria("maintenance.mnt_date = '".$end_date."'");
+            }
+
             $query = $table->getDisplayFieldCriteria($query);
             if (!empty($query)) $table->setCriteria($query);
-            
+
         	$items = $table->getAll($start, $limit, $sort, $dir);
         	$totalcount = $table->countAll();
-    
+
         	$data['items'] = $items;
         	$data['success'] = true;
         	$data['total'] = $totalcount;
-        	    	
+
         }catch (Exception $e) {
             $data['message'] = $e->getMessage();
         }
@@ -75,7 +87,7 @@ class Maintenance_controller {
     	$ci = & get_instance();
 		$ci->load->model('maintenance');
 		$table = $ci->maintenance;
-				
+
 		$data = array('items' => array(), 'success' => false, 'message' => '');
 
 		$jsonItems = getVarClean('items', 'str', '');
@@ -93,17 +105,17 @@ class Maintenance_controller {
 			$numItems = count($items);
 			for($i=0; $i < $numItems; $i++){
         		try{
-        		    
+
         		    $table->db->trans_begin(); //Begin Trans
-                	
+
                     	$table->setRecord($items[$i]);
                     	$insert_id = $table->create();
-            		            		
+
             		$table->db->trans_commit(); //Commit Trans
-            		
+
             		$items[$i] = $table->get($insert_id);
         		}catch(Exception $e){
-        		    
+
         		    $table->db->trans_rollback(); //Rollback Trans
         			$errors[] = $e->getMessage();
         		}
@@ -121,19 +133,19 @@ class Maintenance_controller {
 
 			try{
 			    $table->db->trans_begin(); //Begin Trans
-			    	
+
         	        $table->setRecord($items);
             	    $insert_id = $table->create();
-                
+
                 $table->db->trans_commit(); //Commit Trans
-                
+
     	        $data['success'] = true;
     	        $data['message'] = 'Data berhasil disimpan';
-        
+
 	            $data['items'] = $table->get($insert_id);
 	        }catch (Exception $e) {
 	            $table->db->trans_rollback(); //Rollback Trans
-	            
+
 	            $data['message'] = $e->getMessage();
                 $data['items'] = $items;
 	        }
@@ -167,16 +179,16 @@ class Maintenance_controller {
 			for($i=0; $i < $numItems; $i++){
         		try{
         		    $table->db->trans_begin(); //Begin Trans
-        		    
+
                 		$table->setRecord($items[$i]);
                 		$table->update();
-                		
+
                     $table->db->trans_commit(); //Commit Trans
-                    
+
             		$items[$i] = $table->get($items[$i][$table->pkey]);
         		}catch(Exception $e){
         		    $table->db->trans_rollback(); //Rollback Trans
-        		    
+
         			$errors[] = $e->getMessage();
         		}
         	}
@@ -193,19 +205,19 @@ class Maintenance_controller {
 
 			try{
 			    $table->db->trans_begin(); //Begin Trans
-			    
+
     	        	$table->setRecord($items);
         	        $table->update();
-                
+
                 $table->db->trans_commit(); //Commit Trans
-                
+
     	        $data['success'] = true;
     	        $data['message'] = 'Data berhasil diupdate';
 
 	            $data['items'] = $table->get($items[$table->pkey]);
 	        }catch (Exception $e) {
 	            $table->db->trans_rollback(); //Rollback Trans
-	            
+
 	            $data['message'] = $e->getMessage();
                 $data['items'] = $items;
 	        }
@@ -227,7 +239,7 @@ class Maintenance_controller {
 
 		try{
 		    $table->db->trans_begin(); //Begin Trans
-		    
+
 			$total = 0;
             if (is_array($items)){
                 foreach ($items as $key => $value){
@@ -250,9 +262,9 @@ class Maintenance_controller {
 
             $data['success'] = true;
             $data['message'] = $total.' Data berhasil dihapus';
-            
+
             $table->db->trans_commit(); //Commit Trans
-            
+
         }catch (Exception $e) {
             $table->db->trans_rollback(); //Rollback Trans
             $data['message'] = $e->getMessage();
