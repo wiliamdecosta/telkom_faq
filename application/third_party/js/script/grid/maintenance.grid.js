1b562e284c33a882c858af7bd6a17730ed136ca1
wiliamdecosta <wiliamdecosta@gmail.com>
2016-05-09 12:42:07 +0700

    ''

--- application/third_party/js/script/grid/maintenance.grid.js
+++ application/third_party/js/script/grid/maintenance.grid.js
@@ -9,7 +9,7 @@ Tracking.grid.maintenance = Ext.extend(Webi.grid.GridPanel, {
     viewConfig:{forceFit:false},
     initComponent : function() {
         this.store = new Tracking.store.maintenance();
-        
+
         this.columns = [
             {header: Tracking.properties.mnt_id, hidden: true, sortable: true, dataIndex: 'mnt_id'},
 			{header: Tracking.properties.app_name, hidden: false, sortable: true, dataIndex: 'app_name', width:266},
@@ -41,18 +41,25 @@ Tracking.grid.maintenance = Ext.extend(Webi.grid.GridPanel, {
         };
         return defaultData;
     },
-    
+
     buildTopToolbar : function() {
         var buttons = [];
-        
+
         this.labelFilter = new Ext.form.Label({text:'Filter Maintenance :'});
-                
+
+        this.labelDate = new Ext.form.Label({text:'Date :'});
+        this.startDate = new Ext.form.DateField({emptyText:'From'});
+        this.toDate = new Ext.form.DateField({emptyText:'To'});
+
         this.cmbApp = new Tracking.combo.application({emptyText:'Application', width:254});
         this.cmbApp.on('select', this.onSearch, this);
-        
+
+        this.startDate.on('select',this.onSearch,this);
+        this.toDate.on('select',this.onSearch,this);
+
         this.txtSearch = new Ext.form.TextField({emptyText:'Search...', width: 200});
         this.txtSearch.on('specialkey',this.onSearchText,this);
-        
+
         if (this.enableAdd === true){
             buttons.push({
                 itemId: 'btnNew',
@@ -74,7 +81,7 @@ Tracking.grid.maintenance = Ext.extend(Webi.grid.GridPanel, {
                 scope: this
             });
         }
-        
+
         if (this.enableDelete === true){
             if (buttons.length > 1) buttons.push('-');
             buttons.push({
@@ -85,7 +92,7 @@ Tracking.grid.maintenance = Ext.extend(Webi.grid.GridPanel, {
                 disabled: true,
                 scope: this
             });
-        }        
+        }
 
         if (this.usePaging === false){
             if (buttons.length > 1) buttons.push('-');
@@ -99,57 +106,107 @@ Tracking.grid.maintenance = Ext.extend(Webi.grid.GridPanel, {
                 scope: this
             });
         }
-        
+
+        buttons.push('-');
+        buttons.push({
+            itemId: 'btnExcel',
+            text: 'Print Excel',
+            handler: this.onPrintExcel,
+            scope: this
+        });
+
         buttons.push('->');
         buttons.push(this.labelFilter);
         buttons.push(' ');
         buttons.push(this.cmbApp);
         buttons.push(' ');
+        buttons.push(this.startDate);
+        buttons.push(this.toDate);
+        buttons.push(' ');
         buttons.push(this.txtSearch);
-        
+
         buttons.push({
             itemId: 'btnReset',
             text: 'Reset',
             handler: this.onReset,
             scope: this
-        });        
-        
+        });
+
         return buttons;
     },
-    
+
+    onPrintExcel: function() {
+        if( Ext.isEmpty(this.cmbApp.getValue() )) {
+            Ext.Msg.show({
+               title:'Info',
+               msg: 'Application Harus Diisi',
+               buttons: Ext.Msg.OK,
+               animEl: 'elId',
+               icon: Ext.MessageBox.INFO
+            });
+            return;
+        }
+
+        var start_date = Ext.util.Format.date(this.startDate.getValue(), 'Y-m-d');
+        var end_date = Ext.util.Format.date(this.toDate.getValue(), 'Y-m-d');
+
+        var excel_url = BASE_URL+'print_excel/download_maintenance';
+        excel_url += '/'+this.cmbApp.getValue();
+
+        if(!Ext.isEmpty(start_date))
+            excel_url += '/'+ start_date;
+        if(!Ext.isEmpty(end_date))
+            excel_url += '/' + end_date;
+
+        location.href = excel_url;
+    },
+
     onReset: function() {
     	this.store.baseParams = {};
     	this.store.load();
-    	
+
     	this.cmbApp.reset();
-    	this.txtSearch.reset();		
+        this.startDate.reset();
+        this.toDate.reset();
+    	this.txtSearch.reset();
     },
-    
+
     getSearchParams: function() {
     	var app_id = this.cmbApp.getValue();
     	var search_text = this.txtSearch.getValue();
-				
+
+        var start_date = Ext.util.Format.date(this.startDate.getValue(), 'Y-m-d');
+        var end_date = Ext.util.Format.date(this.toDate.getValue(), 'Y-m-d');
+
 		var p = {};
 		if(!Ext.isEmpty(app_id)) {
 			p['app_id'] = app_id;
 		}
-        
+
         if(!Ext.isEmpty(search_text)) {
 			p['search_text'] = search_text;
 		}
-		
+
+        if(!Ext.isEmpty(start_date)) {
+            p['start_date'] = start_date;
+        }
+
+        if(!Ext.isEmpty(end_date)) {
+            p['end_date'] = end_date;
+        }
+
 		return p;
     },
-    
+
     onSearch: function() {
     	var params = this.getSearchParams();
     	this.store.baseParams = params;
     	this.store.load();
     },
-    
+
     onSearchText: function(field, e) {
     	if(e.getKey() == e.ENTER) {
-    		this.onSearch();	
+    		this.onSearch();
     	}
     }
 });
