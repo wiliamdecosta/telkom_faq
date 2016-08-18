ab73bc70963d2a8bd966bdee30b8e294db7c0416
wiliamdecosta <wiliamdecosta@gmail.com>
2016-08-18 17:21:08 +0700

    ''

--- application/controllers/print_excel.php
+++ application/controllers/print_excel.php
@@ -100,8 +100,85 @@ class Print_excel extends CI_Controller {
         exit;
     }
 
+    function report_maintenance() {
+        check_login();
+
+        $app_id = $this->input->get('application_id');
+        $start_date = $this->input->get('start_date');
+        $end_date = $this->input->get('end_date');
+        $status_pengerjaan = $this->input->get('status_pengerjaan');
+
+        $this->load->model('application');
+        $App = $this->application;
+
+        $this->load->model('maintenance');
+        $Maintenance = $this->maintenance;
+
+        $output = '';
+        if(!empty($app_id)) {
+            $item_app = $App->get($app_id);
+            $output .= '<h3>Nama Aplikasi : '.$item_app['app_name'].'</h3>';
+            $Maintenance->setCriteria('maintenance.app_id = '.$app_id);
+        }else {
+            $output .= '<h3>Nama Aplikasi : Semua Aplikasi</h3>';
+        }
+
+        if(!empty($status_pengerjaan)) {
+            $output .= "<h3>Status Pengerjaan : ".$this->getStatus($status_pengerjaan);
+            $Maintenance->setCriteria("maintenance.mnt_id IN ( select mnt_id from track_maintenance_detail where mnt_det_status = '".$status_pengerjaan."')");
+        }else {
+            $output .= "<h3>Status Pengerjaan : Semua";
+        }
+
+        if(!empty($start_date) && !empty($end_date)) {
+            $Maintenance->setCriteria("(maintenance.mnt_date between '".$start_date."' and '".$end_date."')");
+        }elseif(!empty($start_date)) {
+            $Maintenance->setCriteria("maintenance.mnt_date = '".$start_date."'");
+        }elseif(!empty($end_date)) {
+            $Maintenance->setCriteria("maintenance.mnt_date = '".$end_date."'");
+        }
+
+        $items_maintenance = $Maintenance->getAll(0,-1,'app_name, mnt_date','asc');
+
+        $output .= '<table border="1">';
+        $output .= '<tr>
+                        <th>No</th>
+                        <th>Application</th>
+                        <th>Deskripsi</th>
+                        <th>Tgl Maintenance</th>
+                        <th>Diminta Oleh</th>
+                        <th>PM</th>
+                        <th>SE</th>
+                        <th>SA</th>
+                        <th>PRG</th>
+                        <th>DOC</th>
+                    </tr>';
+
+        $no_urut_m = 1;
+        foreach($items_maintenance as $item) {
+            $output.= '<tr>
+                        <td valign="top">'.$no_urut_m++.'</td>
+                        <td valign="top">'.$item['app_name'].'</td>
+                        <td valign="top">'.$item['mnt_desc'].'</td>
+                        <td valign="top" align="center">&nbsp;'.$item['mnt_date'].'</td>
+                        <td valign="top">'.$item['mnt_by'].'</td>
+                        <td valign="top">&nbsp;</td>
+                        <td valign="top">&nbsp;</td>
+                        <td valign="top">&nbsp;</td>
+                        <td valign="top">&nbsp;</td>
+                        <td valign="top">&nbsp;</td>
+                    </tr>';
+        }
+        startExcel('maintenance_report.xls');
+        echo $output;
+        exit;
+    }
+
+
     function getStatus($val) {
-        $status = array('B'=> 'Belum Dikerjakan',
+        $status = array(
+                   '' => 'Semua Status',
+                   'B'=> 'Belum Dikerjakan',
                    'P' => 'Pending',
                    'I' => 'In Progress',
                    'C' => 'Closed');
